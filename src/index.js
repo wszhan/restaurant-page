363f7502c131b7eb925d409d7b269556916feee4
wszhan <wszhan@@users.noreply.github.com>
2023-02-17 14:17:17 -0500

    generate nav bar and footer dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.css';
+
+const body = document.body;
+const contentDivContainer = document.getElementById('content');
+const initialTabContents = [
+    'HOME', 'MENU', 'ABOUT', 'CONTACT'
+];
+
+function generateTab(tabName, header) {
+    if (!tabName) {
+        throw new Error('tabName must not be null');
+    }
+    if (!header) {
+        throw new Error('header must not be null');
+    }
+
+    const tab = document.createElement('div');
+    tab.setAttribute('id', `${tabName.toLowerCase()}-tab`);
+    tab.classList.add('nav-tab');
+    const spanElement = document.createElement('span');
+    spanElement.innerText = tabName;
+    tab.appendChild(spanElement);
+    header.appendChild(tab);
+}
+
+(function init() {
+    (function generateNavBar() {
+        const header = document.createElement('header');
+        initialTabContents.forEach((tabName) => {
+            generateTab(tabName, header);
+        });
+        body.insertBefore(header, contentDivContainer);
+    })();
+    (function generateFooter() {
+        const footer = document.createElement('footer');
+        footer.innerText = 'Copyright ©️ 2023 OO Studio';
+        body.appendChild(footer);
+    })();
+})();
